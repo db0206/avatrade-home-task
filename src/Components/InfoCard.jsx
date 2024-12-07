b0cbbdbbf772c269771ef33548514aa967f04d9f
Doron <doronbrgr@yahoo.com>
2024-12-07 21:02:04 +0200

    finished

--- src/Components/InfoCard.jsx
+++ src/Components/InfoCard.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 
-const InfoCard = ({topText, mainText, bottomText, className}) => {
+const InfoCard = ({ topText, mainText, bottomText, className }) => {
   return (
-    <div className={`relative border border-white/50 bg-transparent/20 backdrop-blur-sm p-6 px-8 rounded-lg max-w-full overflow-hidden shadow-lg ${className}`}>
+    <div
+      className={`  relative border border-white/50 bg-transparent/20 backdrop-blur-sm p-6 px-8 max-w-full overflow-hidden shadow-inner shadow-gray-300 transition duration-300 hover:shadow-[0_0_25px_rgba(255,255,0,0.5)] ${className} `}
+    >
       {topText && <p className="text-sm text-white break-words">{topText}</p>}
-      {mainText && <h1 className="text-5xl font-bold text-yellow-200 break-words">{mainText}</h1>}
-      {bottomText && <p className="text-sm text-white break-words">{bottomText}</p>}
+      {mainText && (
+        <h1 className="text-5xl font-bold text-yellow-200 break-words">
+          {mainText}
+        </h1>
+      )}
+      {bottomText && (
+        <p className="text-sm text-white break-words">{bottomText}</p>
+      )}
     </div>
   );
 };
--- src/Pages/HeroPage.jsx
+++ src/Pages/HeroPage.jsx
@@ -61,34 +61,46 @@ const HeroPage = () => {
         />
       </div>
 
+
       {/*gentle black overlay */}
       <div className="absolute top-0 left-0 w-full h-full bg-black opacity-25 z-[-1]"></div>
 
       {/*page content */}
+
       <div className="flex flex-col justify-center items-center min-h-screen gap-y-4 px-8 mx-4">
         {/*main text */}
-        <h1 className="text-7xl text-white">
+        <h1 className=" text-4xl sm:text-5xl lg:text-7xl  lg:mb-8 text-white max-w-screen-lg ">
           {titleText ? getStyledText(titleText) : "Loading..."}
         </h1>
 
         {/*cards*/}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-0 lg:gap-6">
-          <InfoCard mainText="$0.19" bottomText="Per Micro contract" />
-          <InfoCard mainText="$0.49" bottomText="Per Standard contract" />
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-0 lg:gap-6 lg:mb-8">
+          <InfoCard
+            mainText="$0.19"
+            bottomText="Per Micro contract"
+            className="rounded-tl-[36px]"
+          />
+          <InfoCard
+            mainText="$0.49"
+            bottomText="Per Standard contract"
+            className="lg:rounded-tl-[36px]"
+          />
           <InfoCard
             topText="Up to"
             mainText="3% interest"
             bottomText="on free margin"
-            className="sm:col-span-2 lg:col-span-1"
+            className="sm:col-span-2 lg:col-span-1 lg:rounded-tl-[36px]"
           />
         </div>
 
         {/*buttons */}
-        <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 gap-4">
-          <button className="bg-yellow-200 text-black py-4 px-20  ">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+          <button className="bg-yellow-200 text-black py-4 px-20 transition transform hover:scale-110 active:scale-90 ">
             {buttonText || "Loading..."}
           </button>
-          <button className="text-white opacity-60">Free Demo</button>
+          <button className="text-white opacity-60 transition transform hover:scale-110 active:scale-90">
+            Free Demo
+          </button>
         </div>
       </div>
     </div>
